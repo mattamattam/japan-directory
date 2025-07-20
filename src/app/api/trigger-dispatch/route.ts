4b2ac6435209198fdb168364653fb0d6549b386c
Matt Armstrong <mattamattam@users.noreply.github.com>
2025-07-20 14:52:02 -0700

    hacky fix

--- src/app/api/trigger-dispatch/route.ts
+++ src/app/api/trigger-dispatch/route.ts
@@ -2,7 +2,7 @@
 import { NextResponse } from "next/server";
 
 export async function POST() {
-  const githubToken = process.env.NEXT_PUBLIC_GH_TOKEN;
+  const githubToken = "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"; // process.env.NEXT_PUBLIC_GH_TOKEN;
 
   if (!githubToken) {
     return NextResponse.json(
